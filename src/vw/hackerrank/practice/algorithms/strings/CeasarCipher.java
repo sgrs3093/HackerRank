6632c9d723e5924aea28d771e968af6ab0d4d13b
Vivek Wisdom <ervivekwisdom@gmail.com>
2017-01-21 21:35:33 +0530

    Added string practice problem

---
+++ src/vw/hackerrank/practice/algorithms/strings/CeasarCipher.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package vw.hackerrank.practice.algorithms.strings;
+
+import java.util.Scanner;
+
+/**
+ * @author vivek
+ *
+ */
+public class CeasarCipher {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int len = in.nextInt();
+		char[] str = in.next().toCharArray();
+		int inc = in.nextInt() % 26;
+		for (int i = 0; i < len; i++) {
+			int c = str[i];
+			if ((c <= 'z' && c >= 'a')) {
+				c += inc;
+				if (c > 'z') {
+					c = ('a' - 1) + (c % 'z');
+				}
+			} else if ((c <= 'Z' && c >= 'A')) {
+				c += inc;
+				if (c > 'Z') {
+					c = ('A' - 1) + (c % 'Z');
+				}
+			}
+			System.out.print((char) c);
+		}
+	}
+}
